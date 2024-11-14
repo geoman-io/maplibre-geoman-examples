64493684ac517f1240b1dcaebc6322aa47822920
mscno <msc@parap.ly>
2024-11-14 13:42:55 +0100

    add simple example

---
+++ maplibre-simple-starter/src/main.ts
@@ -0,0 +1,80 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+import 'maplibre-gl/dist/maplibre-gl.css';
+import '@geoman-io/maplibre-geoman-free/dist/maplibre-geoman.css';
+
+import ml from 'maplibre-gl';
+import { Geoman, type GmOptionsPartial } from '@geoman-io/maplibre-geoman-free';
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>MapLibre-Geoman Demo map
+        <div id="dev-map"></div>
+  </div>
+`
+
+const mapLibreStyle: ml.StyleSpecification = {
+  version: 8,
+  glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
+  sources: {
+    'osm-tiles': {
+      type: 'raster',
+      tiles: [
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      ],
+      tileSize: 256,
+      attribution: '© OpenStreetMap contributors',
+    },
+  },
+  layers: [
+    {
+      id: 'osm-tiles-layer',
+      type: 'raster',
+      source: 'osm-tiles',
+      minzoom: 0,
+      maxzoom: 19,
+    },
+  ],
+};
+
+const map = new ml.Map({
+  container: 'dev-map',
+  style: mapLibreStyle,
+  center: [0, 51],
+  zoom: 5,
+});
+
+const gmOptions: GmOptionsPartial = {
+  // geoman options here
+};
+
+// create a new geoman instance
+const geoman = new Geoman(map, gmOptions);
+
+// callback when geoman is fully loaded
+map.on('gm:loaded', () => {
+  console.log('Geoman fully loaded');
+
+  // Here you can add your geojson shapes for example
+  const shapeGeoJson = {
+    type: 'Feature',
+    geometry: { type: 'Point', coordinates: [0, 51] },
+    properties: {},
+  };
+  // add a geojson shape to the map
+  geoman.features.addGeoJsonFeature({ shapeGeoJson });
+
+
+  const shapeGeoJson2 = {
+    type: 'Feature',
+    geometry: { type: 'Point', coordinates: [3, 52] },
+    properties: {},
+  };
+  // geoman instance is also available on the map object
+  map.gm?.features.addGeoJsonFeature({ shapeGeoJson: shapeGeoJson2 });
+
+});
