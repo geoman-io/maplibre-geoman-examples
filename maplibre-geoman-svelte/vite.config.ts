5aeb9cccd13181e317df909bafbd81d1043d38b2
mscno <msc@parap.ly>
2024-12-18 14:50:45 +0100

    clean up external deps vite

--- maplibre-geoman-svelte/vite.config.ts
+++ maplibre-geoman-svelte/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { externalizeDeps } from "vite-plugin-externalize-deps"
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(),externalizeDeps()],
+  plugins: [svelte()],
 })
